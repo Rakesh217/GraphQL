2192e7627930840173ff14558898370e36f912b0
Rakesh <sairakesh217@gmail.com>
2020-04-24 15:06:28 -0400

    commit

--- GRAPHQL-REACT-DEMO/app.js
+++ GRAPHQL-REACT-DEMO/app.js
@@ -9,6 +9,17 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -23,7 +34,7 @@ app.use(
 mongoose.connect(
     `mongodb+srv://user:xziS4WoLu0MqFXxC@cluster0-ewrgw.mongodb.net/events-react-dev?retryWrites=true&w=majority`
 ).then(() => {
-    app.listen(3000);
+    app.listen(8000);
 }).catch(err => {
     console.log(err);
 });
